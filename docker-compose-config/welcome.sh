4919c12f3476b8bd6cc07504366773fb49b3a600
Duncan Brown <duncanjbrown@gmail.com>
2025-06-24 16:07:40 +0100

    Add welcome container

---
+++ docker-compose-config/welcome.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+figlet -f future.flf CADDY
+
+echo "Caddy is running"
+echo
+echo "Model:    http://localhost:8080"
+echo "Client:   http://localhost:8081"
+echo "Frontend: http://localhost:8082"
