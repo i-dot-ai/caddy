693d5924d59ba4df255a98af3db4e96495e4fb5b
Kevin Etchells <kevetchells@hotmail.com>
2025-07-17 10:38:35 +0100

    Display the file names of the files that are uploading

--- frontend/src/components/upload-info.ts
+++ frontend/src/components/upload-info.ts
@@ -2,29 +2,39 @@ const UploadInfo = class extends HTMLElement {
 
   private form: HTMLFormElement | null = null;
 
+  private fileInput: HTMLInputElement | null = null;
+
   connectedCallback() {
 
-    this.form = this.closest('form');
+    this.fileInput = document.querySelector(`#${this.getAttribute('input-id')}`);
+    this.form = this.fileInput?.closest('form') as HTMLFormElement | null;
+
     this.form?.addEventListener('submit', async(evt) => {
       evt.preventDefault();
-      this.form?.classList.add('form-submitted');
-      const files = await this.#uploadFiles();
-      const notification = files.length === 1 ? `File <strong>${files[0].name}</strong> uploaded` : `<strong>${files.length}</strong> files uploaded`;
-      window.location.href = `${this.getAttribute('resources-page') || window.location.href}?notification=${encodeURI(notification)}`;
+      await this.#uploadFiles();
+      this.querySelector('[hidden]')?.classList.add('govuk-button');
     });
 
   }
 
 
-  #render() {
+  #render(files: File[]) {
+
+    const notification = files.length === 1 ? `File <strong>${files[0].name}</strong> uploaded` : `<strong>${files.length}</strong> files uploaded`;
 
     this.innerHTML = `
-      <div class="upload-info__ellipsis govuk-body govuk-!-font-weight-bold">
-        <span id="upload-info__count" aria-live="assertive">Uploading file(s)</span>
-        <span aria-hidden="true">.</span>
-        <span aria-hidden="true">.</span>
-        <span aria-hidden="true">.</span>
-      </div>
+      <h2 class="govuk-heading-m govuk-!-margin-top-7">Uploading files</h2>
+      <ul class="govuk-task-list">
+        ${files.map((file) => `
+          <li class="govuk-task-list__item">
+            <div class="govuk-task-list__name-and-hint">${file.name}</div>
+            <div class="govuk-task-list__status">
+              <strong class="govuk-tag govuk-tag--grey">Queued</strong>
+            </div>
+          </li>
+        `).join('')}
+      </ul>
+      <a href="../resources?notification=${notification}" hidden>Back to resources page</a>
     `;
 
     this.classList.add('upload-info');
@@ -39,30 +49,37 @@ const UploadInfo = class extends HTMLElement {
   async #uploadFiles() {
 
     // get files
-    const fileInput = document.querySelector(`#${this.getAttribute('input-id')}`) as HTMLInputElement;
     const collectionId = this.getAttribute('collection') || '';
-    const files = [...fileInput.files || []];
+    const files = [...this.fileInput?.files || []];
 
-    this.#render();
+    this.#render(files);
+    const fileStatuses = this.querySelectorAll('.govuk-tag');
 
     // send each file one at a time
     for (let fileIndex = 0; fileIndex < files.length; fileIndex++) {
-      (this.querySelector('#upload-info__count') as HTMLElement).textContent = `Uploading file ${fileIndex + 1} of ${files.length}`;
+
       const file = files[fileIndex];
-      console.log(`Uploading: ${file.name}`);
+
+      fileStatuses[fileIndex].className = 'govuk-tag govuk-tag--blue';
+      fileStatuses[fileIndex].textContent = 'Uploading';
+
       const formData = new FormData();
       formData.append('collection', collectionId);
-      formData.append(fileInput.name, file);
+      formData.append(this.fileInput?.name || '', file);
       const response = await fetch(this.form?.action || '', {
         method: 'POST',
         body: formData,
       });
+
       if (!response.ok) {
         console.error(`Upload failed for ${file.name}: ${response.statusText}`);
+        fileStatuses[fileIndex].className = 'govuk-tag govuk-tag--red';
+        fileStatuses[fileIndex].textContent = 'Error';
+      } else {
+        fileStatuses[fileIndex].className = 'govuk-tag govuk-tag--green';
+        fileStatuses[fileIndex].textContent = 'Complete';
       }
     }
-
-    return files;
 
   }
 
