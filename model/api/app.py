2158a21761d400fd3bdfaac8b5fe2d827feb7506
Duncan Brown <duncanjbrown@gmail.com>
2025-07-17 18:24:26 +0100

    Whitespace

--- model/api/app.py
+++ model/api/app.py
@@ -141,11 +141,7 @@ async def query_endpoint(
         )
 
     try:
-        return await search_collection(
-            collection.id,
-            request.query,
-            session=session
-        )
+        return await search_collection(collection.id, request.query, session=session)
 
     except Exception:
         logger.exception(
