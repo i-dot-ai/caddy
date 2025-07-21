7a3f56a776f98ff1f5f09f1061f9f5897c66d775
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-07-21 07:34:05 +0100

    Added sort for email for users list and collection name for collections list

--- model/api/rest_app.py
+++ model/api/rest_app.py
@@ -704,7 +704,7 @@ def get_collections_user_roles(
         select(UserCollection, User.email.label("user_email"))
         .where(UserCollection.collection_id == collection_id)
         .join(User, UserCollection.user_id == User.id)
-        .order_by(UserCollection.created_at)
+        .order_by(UserCollection.user.email)
         .offset(page_size * (page - 1))
         .limit(page_size)
     )
