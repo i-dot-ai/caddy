488f794b0ac369162c6187a9a502ae43512db924
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-07-23 13:49:11 +0100

    Remove commented out lines

--- model/tests/test_file_upload.py
+++ model/tests/test_file_upload.py
@@ -60,8 +60,6 @@ def test_file_upload(client, tmp_path, admin_user):
         f"{config.s3_prefix}/{collection_id}/{item['id']}/"
         for item in response.json()["resources"]
     }
-    # for resource_id in resource_ids:
-    #     assert resource_id in s3_keys
     assert all(
         any(resource_id in s3_key for s3_key in s3_keys) for resource_id in resource_ids
     )
