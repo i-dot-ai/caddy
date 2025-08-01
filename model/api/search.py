d3abbdbd32779d21ba5a731522270a416bfbafa4
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-08-01 11:23:41 +0100

    Add download_url to tool response metadata

--- model/api/search.py
+++ model/api/search.py
@@ -18,7 +18,18 @@ def build_document(document: Document, collection_id, session):
         url = config.resource_url_template.format(
             collection_id=collection_id, resource_id=resource.id
         )
+        s3_client = config.s3_client
+
+        s3_url = s3_client.generate_presigned_url(
+            "get_object",
+            Params={
+                "Bucket": config.data_s3_bucket,
+                "Key": f"{config.s3_prefix}/{resource.collection_id}/{resource.id}/{resource.filename}",
+            },
+            ExpiresIn=3600,
+        )
         document.metadata["url"] = url
+        document.metadata["download_url"] = s3_url
 
     return document
 
