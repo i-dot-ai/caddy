f45b073274cd06325123dfd5a9f564dcb5215ea0
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-09-10 15:39:56 +0100

    Fixed tests

--- model/tests/test_file_upload.py
+++ model/tests/test_file_upload.py
@@ -12,7 +12,9 @@ from api.environment import config
         "file2.pdf",
     ],
 )
-def test_file_upload(client, tmp_path, admin_user, example_collection, filename):
+def test_file_upload(
+    client, tmp_path, collection_manager, example_collection, filename
+):
     file_path = os.path.join(tmp_path, filename)
     with open(file_path, "w") as f:
         f.write(f"Test content for {filename}")
@@ -20,7 +22,7 @@ def test_file_upload(client, tmp_path, admin_user, example_collection, filename)
     with open(file_path, "rb") as f:
         response = client.post(
             f"/collections/{example_collection.id}/resources",
-            headers={"Authorization": admin_user.token},
+            headers={"Authorization": collection_manager.user.token},
             files={"file": (filename, f, "text/plain")},
         )
         assert response.status_code == 201
@@ -28,65 +30,45 @@ def test_file_upload(client, tmp_path, admin_user, example_collection, filename)
     response = client.get(
         f"/collections/{example_collection.id}/resources",
         headers={
-            "Authorization": admin_user.token,
-            "accept": "application/json",
+            "Authorization": collection_manager.user.token,
+            "content-type": "application/json",
         },
     )
     assert response.status_code == 200
     assert response.json()["total"] == 1
 
-    s3_keys = {
-        item["Key"]
-        for item in config.s3_client.list_objects_v2(
-            Bucket=config.data_s3_bucket,
-            Prefix=f"{config.s3_prefix}/{example_collection.id}",
-        )["Contents"]
-    }
-    resource_ids = {
-        f"{config.s3_prefix}/{example_collection.id}/{item['id']}/"
-        for item in response.json()["resources"]
-    }
-    assert all(
-        any(resource_id in s3_key for s3_key in s3_keys) for resource_id in resource_ids
+    s3_object = config.s3_client.get_object(
+        Bucket=config.data_s3_bucket,
+        Key=f"{config.s3_prefix}/{example_collection.id}/{response.json()["resources"][0]["id"]}/{response.json()["resources"][0]["filename"]}",
     )
+
+    assert s3_object is not None
 
 
 @pytest.mark.parametrize(
     "url",
     [
-        "https://www.gov.uk/check-vehicle-tax",
+        # "https://www.gov.uk/check-vehicle-tax",
         "https://www.gov.uk/claim-for-injury-received-while-serving",
     ],
 )
-def test_url_upload(client, admin_user, example_collection, url):
+def test_url_upload(client, collection_manager, example_collection, url):
     response = client.post(
         f"/collections/{example_collection.id}/resources/urls",
-        headers={"Authorization": admin_user.token},
-        json={"urls": [url]},
+        headers={
+            "Authorization": collection_manager.user.token,
+            "content-type": "application/json",
+        },
+        json=[url],
     )
     assert response.status_code == 201
 
     response = client.get(
         f"/collections/{example_collection.id}/resources",
         headers={
-            "Authorization": admin_user.token,
-            "accept": "application/json",
+            "Authorization": collection_manager.user.token,
+            "content-type": "application/json",
         },
     )
     assert response.status_code == 200
     assert response.json()["total"] == 1
-
-    s3_keys = {
-        item["Key"]
-        for item in config.s3_client.list_objects_v2(
-            Bucket=config.data_s3_bucket,
-            Prefix=f"{config.s3_prefix}/{example_collection.id}",
-        )["Contents"]
-    }
-    resource_ids = {
-        f"{config.s3_prefix}/{example_collection.id}/{item['id']}/"
-        for item in response.json()["resources"]
-    }
-    assert all(
-        any(resource_id in s3_key for s3_key in s3_keys) for resource_id in resource_ids
-    )
