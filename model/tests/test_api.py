47087c0cc9f28cfd64e27d87caaff7cc54b84f9a
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-10-16 09:13:44 +0100

    Fix test

--- model/tests/test_api.py
+++ model/tests/test_api.py
@@ -15,7 +15,7 @@ from api.models import (
     UserCollectionWithEmail,
     UserRoleList,
 )
-from api.types import Chunks, CollectionsDto
+from api.types import Chunks, CollectionsDto, ResourceChunks
 
 s3_client = config.s3_client
 CWD = os.path.dirname(os.path.abspath(__file__))
@@ -119,36 +119,39 @@ def test_get_resource_documents(client, collection_manager, many_documents, admi
     assert response.status_code == 200
     actual_result = Chunks.model_validate(response.json())
     #  I EXPECT the 4th and 5th documents to be returned
-    expected_result = Chunks(
-        page=3,
-        page_size=2,
-        total=10,
-        collection_id=collection_manager.collection_id,
-        resource_id=resource_id,
-        documents=[
-            Document(
-                id="00000000-0000-0000-0000-000000000024",
-                metadata={
-                    "filename": "filename-02",
-                    "resource_id": str(resource_id),
-                    "content_type": "text/plain",
-                    "created_at": "2001-01-01T01:01:00",
-                    "chunk_order": 4,
-                },
-                page_content="resource=2, document=4",
-            ),
-            Document(
-                id="00000000-0000-0000-0000-000000000025",
-                metadata={
-                    "filename": "filename-02",
-                    "resource_id": str(resource_id),
-                    "content_type": "text/plain",
-                    "created_at": "2001-01-01T01:01:00",
-                    "chunk_order": 5,
-                },
-                page_content="resource=2, document=5",
-            ),
-        ],
+    expected_result = ResourceChunks(
+        url=f"http://localhost:4322/collections/{collection_manager.collection_id}/resources/{resource_id}",
+        chunks=Chunks(
+            page=3,
+            page_size=2,
+            total=10,
+            collection_id=collection_manager.collection_id,
+            resource_id=resource_id,
+            documents=[
+                Document(
+                    id="00000000-0000-0000-0000-000000000024",
+                    metadata={
+                        "filename": "filename-02",
+                        "resource_id": str(resource_id),
+                        "content_type": "text/plain",
+                        "created_at": "2001-01-01T01:01:00",
+                        "chunk_order": 4,
+                    },
+                    page_content="resource=2, document=4",
+                ),
+                Document(
+                    id="00000000-0000-0000-0000-000000000025",
+                    metadata={
+                        "filename": "filename-02",
+                        "resource_id": str(resource_id),
+                        "content_type": "text/plain",
+                        "created_at": "2001-01-01T01:01:00",
+                        "chunk_order": 5,
+                    },
+                    page_content="resource=2, document=5",
+                ),
+            ],
+        ),
     )
     assert actual_result == expected_result
 
