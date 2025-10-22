473187570060d096b629eb204b73203bc91c755d
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-10-22 08:58:49 +0100

    Add ECS combination

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -65,7 +65,7 @@ module "model" {
 
   container_port             = local.backend_port
   memory                     = terraform.workspace == "prod" ? 8152 : 4096
-  cpu                        = 1024
+  cpu                        = terraform.workspace == "prod" ? 2048 : 1024
   autoscaling_maximum_target = var.env == "prod" ? 5 : 1
 
   health_check = {
